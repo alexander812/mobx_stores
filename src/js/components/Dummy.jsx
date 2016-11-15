339f8c221ae4f54ca9aa84c29880ae16b6166b84
Alexander Smirnov <a.smirnov@olymptrade.com>
2016-11-15 13:26:22 +0300

    first update

--- src/js/components/Dummy.jsx
+++ src/js/components/Dummy.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import PlatformStore from 'stores/PlatformStore';
+import {observer, Provider} from 'mobx-react'
+
+
 
 var platformStore = new PlatformStore();
 
 
-console.log(['platformStore', platformStore, platformStore.dealFormSum]);
+console.log(['platformStore', platformStore, platformStore.dealFormSum, Provider]);
 
+
+
+
+setTimeout(function(){
+
+    platformStore.dealFormSum = 100;
+
+
+}.bind(this), 3000);
+
+
+
+
+console.log(['observer', observer]);
+
+
+@observer(["platformStore"])
 class Component extends React.Component{
     render(){
-        return <div>Dummy component</div>
+        
+
+        //this.props.platformStore.dealFormSum
+        
+        
+        console.log(['this.props', this.props]);
+        return <div>Dummy component: {this.props.platformStore.dealFormSum}</div>
     }
 }
 
-export default Component;
+
+class Entry extends React.Component{
+    render(){
+        console.log([777]);
+        return  (<Provider platformStore={platformStore}>
+            <Component/>
+        </Provider>)
+
+    }
+}
+/*
+
+ <Provider>
+ <Component/>
+ </Provider>
+ */
+
+
+
+export default Entry;
--- src/js/stores/PlatformStore.js
+++ src/js/stores/PlatformStore.js
@@ -3,8 +3,11 @@ import { observable } from 'mobx';
 class PlatformStore {
     @observable dealFormSum = 0;
     constructor(){
-
+        
         this.dealFormSum = 888;
+        
+        
+        
 
     }
 }
